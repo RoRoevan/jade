fdd600e42ab63b5ceb4c17bee6093690dc31a2a4
Roevan <racmatoza@mymail.mapua.edu.ph>
2026-06-27 13:52:41 +0800

    Suppress PHP 8.5 deprecation warnings to prevent HTML output corruption

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// Suppress deprecation warnings on PHP 8.5+ to prevent HTML output corruption
+ini_set('display_errors', '0');
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 // Copy SQLite database to /tmp so it's writeable at runtime
 $templateDb = __DIR__ . '/../database/database.sqlite';
 $targetDb = '/tmp/database.sqlite';

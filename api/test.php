3b4098403a0c84df5ab7544ee89fa674f887d9e2
Roevan <racmatoza@mymail.mapua.edu.ph>
2026-06-27 14:02:48 +0800

    Update test.php to check autoload.php presence

--- api/test.php
+++ api/test.php
@@ -1,2 +1,7 @@
 <?php
-echo implode(', ', get_loaded_extensions());
+$path = __DIR__.'/../vendor/autoload.php';
+if (file_exists($path)) {
+    echo "vendor/autoload.php EXISTS!\n";
+} else {
+    echo "vendor/autoload.php does NOT exist!\n";
+}

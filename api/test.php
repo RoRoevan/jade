425a66c331c17fa4aca85f6ec2f9895b42a0b39a
Roevan <racmatoza@mymail.mapua.edu.ph>
2026-06-27 14:04:48 +0800

    Update test.php to print memory limit

--- api/test.php
+++ api/test.php
@@ -1,7 +1,2 @@
 <?php
-$path = __DIR__.'/../vendor/autoload.php';
-if (file_exists($path)) {
-    echo "vendor/autoload.php EXISTS!\n";
-} else {
-    echo "vendor/autoload.php does NOT exist!\n";
-}
+echo "Memory Limit: " . ini_get('memory_limit') . "\n";
